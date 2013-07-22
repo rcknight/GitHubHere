ab6ecd0c5e56859e93e991f125cb9ab707ef5fc2
Rich Knight <rich@rich-knight.co.uk>
2013-07-22 11:57:00 +0100

    initial commit

---
+++ gh.hs
@@ -0,0 +1,54 @@
+import System.IO
+import System.Directory (doesDirectoryExist, getCurrentDirectory)
+import System.FilePath
+import System.Cmd
+import Data.List
+import Text.Regex.Posix
+
+parentDirectory :: FilePath -> FilePath
+parentDirectory d = joinPath (init (splitDirectories d))
+
+listAncestors :: FilePath -> [FilePath]
+listAncestors "" = []
+listAncestors path = [path] ++ listAncestors (parentDirectory path)
+
+findGitFolder :: FilePath -> IO (Maybe FilePath)
+findGitFolder "" 	= return Nothing
+findGitFolder path 	= do
+	let gitPath = joinPath [path, ".git"]
+	exists <- doesDirectoryExist gitPath
+	if exists
+	then return (Just gitPath)
+	else findGitFolder (parentDirectory path) 
+
+githubUrl :: FilePath -> IO (Maybe String)
+githubUrl configPath = do
+	results <- parseConfig configPath
+	if (length results) > 0
+	then return (Just ("https://github.com/" ++ ((last (head results)))))
+	else return Nothing
+
+parseConfig :: FilePath -> IO [[String]]
+parseConfig configPath = do 
+	content <- readFile configPath
+	return (content =~ "github.com[:/]([^.]*)")
+
+launchGithubForPath :: FilePath -> IO ()
+launchGithubForPath path = do
+						gitFolder <- findGitFolder path
+						case gitFolder of 
+							Just f  -> launchBrowser (githubUrl (joinPath [f, "config"]))
+							Nothing -> putStrLn "Current folder is not inside a git repository"
+
+--TODO .. change this to just take a string
+launchBrowser url = do 
+	toOpen <- url
+	case toOpen of
+		Just u ->  do 
+			system ("open " ++ u)
+			putStrLn ("Launching " ++ u)
+		Nothing -> putStrLn "No github remote found"
+
+main = do
+	currentFolder <- getCurrentDirectory 
+	launchGithubForPath currentFolder
